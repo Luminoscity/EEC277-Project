37ab82368f8d7aa379f643a377ca149868eb1140
Tim Ambrose <Tim Ambrose>
2017-03-03 00:00:04 -0800

    Fixed GrabTriangle to omit 0,0,0,0 entries and all duplicates within n lines

--- GrabTriangles.cpp
+++ GrabTriangles.cpp
@@ -21,14 +21,19 @@
 #include <vector>
 #include <fstream>
 #include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <algorithm>
 
 #define TIME() (((float)clock() - (float)t1) / CLOCKS_PER_SEC)
+#define COORD_COUNT 4
 
 using std::string;
 using std::vector;
 using std::cout;
 using std::ifstream;
 using std::ofstream;
+using std::stringstream;
 
 void CheckArgs(int argc, char *argv[]);
 #pragma endregion
@@ -41,19 +46,37 @@ int main(int argc, char *argv[]) {
    const char coordString[] = "0x";
    ifstream inFile(argv[1]);
    ofstream outFile(argv[2]);
+   unsigned dupLines = atoi(argv[3]);
    string line;
    clock_t t1 = clock();
 
-   outFile << std::hex;
+   outFile << std::hex << std::fixed << std::setprecision(9);
    int i = 0;
+   vector<uint32_t> zero(COORD_COUNT, 0);
+   vector<uint32_t> coords(COORD_COUNT, 0);
+   vector<vector<uint32_t> > lines;
    while (getline(inFile, line)) {
       size_t pos = line.find(geometryString, 0);
       if (pos != string::npos) {
-         while ((pos = line.find(coordString, pos + 1)) != string::npos) {
-            auto number = std::stoul(line.substr(pos), (size_t*)0, 16);
-            outFile << number << " ";
+         int idx = 0;
+         while ((pos = line.find(coordString, pos + 1)) != string::npos &&
+                idx < COORD_COUNT) {
+            uint32_t number = (uint32_t)std::stoul(line.substr(pos), (size_t*)0, 16);
+            coords[idx++] = number;
          }
-         outFile << "\n";
+         if (dupLines == 0 || (coords != zero &&
+             std::find(lines.begin(), lines.end(), coords) == lines.end())) {
+            for (int j = 0; j < COORD_COUNT; ++j) {
+               float f = *(float *)(&coords[j]);
+               outFile << f << " ";
+            }
+            outFile << "\n";
+         }
+         if (dupLines) {
+            lines.push_back(coords);
+            if (lines.size() > dupLines)
+               lines.erase(lines.begin());
+         }
       }
       if (++i % 100000 == 0) {
          printf("%0.3fs: Processed line: %d\n", TIME(), i);
@@ -67,14 +90,20 @@ int main(int argc, char *argv[]) {
 
 #pragma region Helper Functions
 void CheckArgs(int argc, char *argv[]){
-	if(argc != 3) {
-		fprintf(stderr, "Usage: %s traceFile output_file_name\n", argv[0]);
-		exit(-1);
-	}
-	ifstream file1(argv[1]);
-	if(!file1.good()) {
-		fprintf(stderr, "Could not open file: %s\n", argv[1]);
-		exit(-1);
-	}
+   if(argc != 4) {
+      fprintf(stderr, "Usage: %s traceFile output_file_name duplicate_line_range\n", argv[0]);
+      exit(-1);
+   }
+   ifstream file1(argv[1]);
+   if(!file1.good()) {
+      fprintf(stderr, "Could not open file: %s\n", argv[1]);
+      exit(-1);
+   }
+   if(atoi(argv[3]) < 0) {
+      fprintf(stderr, "duplicate_line_range must be at least 0\n"
+                      "0 to allow all duplicates\n"
+                      "n to eliminate duplicates within n lines\n", argv[1]);
+      exit(-1);
+   }
 }
 #pragma endregion
